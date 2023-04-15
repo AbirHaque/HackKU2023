129745fdf925f5bfdf24e31dd1721669a220d04e
rahulamatapu <rahulpranav0718@gmail.com>
2023-04-15 15:03:56 -0500

    Changed wiki

--- webapp/src/components/Composition/CompositionCard.js
+++ webapp/src/components/Composition/CompositionCard.js
@@ -3,9 +3,12 @@ import React from 'react';
 
 const CompositionCard = ({ title }) => {
   return (
+    <>
+    <h1>Ayy lmao1</h1>
     <li className="title-item">
       {title}
     </li>
+    </>
   );
 };
 
