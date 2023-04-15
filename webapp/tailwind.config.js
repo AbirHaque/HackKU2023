77db83c86d9015df182999112b3f47873232c702
Nikhil Raja <123650960+nr-eecs@users.noreply.github.com>
2023-04-15 16:50:16 -0500

    added tailwind and flowbite for styling

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/flowbite/**/*.js"
-],
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  module.exports = {
 
+    plugins: [
+        require('flowbite/plugin')
+    ]
+
+},
+}
