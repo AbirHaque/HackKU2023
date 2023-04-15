431e84b26b53806004981809e5f60b82cc0d704a
rahulamatapu <rahulpranav0718@gmail.com>
2023-04-15 14:41:29 -0500

    Composition updates

---
+++ webapp/src/components/Composition/CompositionCard.js
@@ -0,0 +1,12 @@
+// src/components/CompositionCard.js
+import React from 'react';
+
+const CompositionCard = ({ title }) => {
+  return (
+    <li className="title-item">
+      {title}
+    </li>
+  );
+};
+
+export default CompositionCard;
