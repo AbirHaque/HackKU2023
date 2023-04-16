f34bc604dc1db0696b03525c7bc8891b921eb15f
Abir Haque <abir.haque.usa@gmail.com>
2023-04-16 02:49:02 -0500

    A

--- webapp/src/components/Composition/CompositionListView.js
+++ webapp/src/components/Composition/CompositionListView.js
@@ -6,25 +6,27 @@ import { Sidebar } from 'flowbite-react';
 import { HiAnnotation } from 'react-icons/hi';
 
 const CompositionListView = () => {
+  var titles=[];
   var url="http://localhost:5000/graphql"+"?"+"query"+"={comprehensions{_id,title}}";
   console.log(url);
   fetch(url).then((response)=>response.json())
   .then((data)=>{
-    var titles=[];
     var comprehensions=data.data.comprehensions;
     for(var i = 0; i < comprehensions.length;i++){
       titles.push(comprehensions[i].title);
     }
-    console.log(data);
-    return (
-      <ul className="title-list">
-        <p class="text-2xl text-gray-900 dark:text-white">Comp Titles</p>
-        {titles.map((title, index) => (
-          <CompositionCard key={index} title={title} />
-        ))}
-      </ul>
-    );
+    console.log(titles);
+
+    
   });
+  return (
+    <ul className="title-list">
+      <p className="text-2xl text-gray-900 dark:text-white">Comp Titles</p>
+      {titles.map((title, index) => (
+        <CompositionCard key={index} title={title} />
+      ))}
+    </ul>
+  );
 };
 
 export default CompositionListView;
