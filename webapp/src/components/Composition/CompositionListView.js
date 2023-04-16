603058d31c8ce4c2e8d766f6bc497b2e4b4c0f49
Abir Haque <abir.haque.usa@gmail.com>
2023-04-16 02:11:15 -0500

    Update CompositionListView.js

--- webapp/src/components/Composition/CompositionListView.js
+++ webapp/src/components/Composition/CompositionListView.js
@@ -1,24 +1,28 @@
 // src/components/CompositionListView.js
 import React from 'react';
+import {useState,useEffect} from "react";
 import CompositionCard from './CompositionCard';
 
 const CompositionListView = () => {
-  const titles = [
-    'Title 1',
-    'Title 2',
-    'Title 3',
-    'Title 4',
-    'Title 5',
-  ];
-
-  return (
-    <ul className="title-list">
-      <p class="text-2xl text-gray-900 dark:text-white">Comp Titles</p>
-      {titles.map((title, index) => (
-        <CompositionCard key={index} title={title} />
-      ))}
-    </ul>
-  );
+  var url="http://localhost:5000/graphql"+"?"+"query"+"={comprehensions{_id,title}}";
+  console.log(url);
+  fetch(url).then((response)=>response.json())
+  .then((data)=>{
+    var titles=[];
+    var comprehensions=data.data.comprehensions;
+    for(var i = 0; i < comprehensions.length;i++){
+      titles.push(comprehensions[i].title);
+    }
+    console.log(data);
+    return (
+      <ul className="title-list">
+        <p class="text-2xl text-gray-900 dark:text-white">Comp Titles</p>
+        {titles.map((title, index) => (
+          <CompositionCard key={index} title={title} />
+        ))}
+      </ul>
+    );
+  });
 };
 
 export default CompositionListView;
